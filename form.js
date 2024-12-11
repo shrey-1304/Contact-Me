92a13eb632aa58988274c4b81a0e4d54d85839e7
shrey-1304 <149804049+shrey-1304@users.noreply.github.com>
2024-12-11 12:56:42 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,39 +1,40 @@
-let number = document.getElementById("pho");
+  let number = document.getElementById("pho");
 
-number.addEventListener("input", (e) =>{
-let input = e.target.value.replace(/\s+/g, '');
+  number.addEventListener("input", (e) =>{
+  let input = e.target.value.replace(/\s+/g, '');
 
-// Add a space after the first 5 digits
-if (input.length > 5) {
-  input = input.slice(0, 5) + ' ' + input.slice(5);
-}
+  // Add a space after the first 5 digits
+  if (input.length > 5) {
+    input = input.slice(0, 5) + ' ' + input.slice(5);
+  }
 
-// Update the input value
-e.target.value = input;
-});
-
-function scrollToTop(){
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth' // This ensures the scrolling is smooth
+  // Update the input value
+  e.target.value = input;
   });
-}
 
-const textarea = document.querySelector('textarea');
-const wordCounter = document.querySelector('#word-counter');
-const charCounter = 250;
+  function scrollToTop(){
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth' // This ensures the scrolling is smooth
+    });
+  }
 
-textarea.addEventListener('input', function () {
-    const currentLength = this.value.length; // Get current character length
-
-    // Update the character counter
-    charCounter.textContent = `${currentLength} / ${charLimit}`;
-
-    // Prevent additional characters from being added beyond the limit
-    if (currentLength > charLimit) {
-        // Trim the content to the allowed character limit
-        this.value = this.value.slice(0, charLimit);
-        charCounter.textContent = `${charLimit} / ${charLimit}`;
-    }
-});
+  const textarea = document.querySelector('textarea');
+  const charCounter = document.querySelector('#char-counter');
+  const charLimit = 250;
+  
+  textarea.addEventListener('input', function () {
+      const currentLength = this.value.length; // Get current character length
+  
+      // Update the character counter
+      charCounter.textContent = `${currentLength} / ${charLimit}`;
+  
+      // Prevent additional characters from being added beyond the limit
+      if (currentLength > charLimit) {
+          // Trim the content to the allowed character limit
+          this.value = this.value.slice(0, charLimit);
+          charCounter.textContent = `${charLimit} / ${charLimit}`;
+      }
+  });
+  
